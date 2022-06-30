85ac5970799a530b4e4f14baa717359a98fd8291
Danil <siroshtandenchik2@gmail.com>
2022-06-30 23:23:12 +0200

    add to git

---
+++ Zad4Egz/Zad4Egz/OknoProduktu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Zad4Egz
+{
+    /// <summary>
+    /// Логика взаимодействия для OknoProduktu.xaml
+    /// </summary>
+    public partial class OknoProduktu : Window
+    {
+        public OknoProduktu(System.Xml.XmlElement produkt)
+        {
+            DataContext = produkt;
+            InitializeComponent();
+        }
+    }
+}
